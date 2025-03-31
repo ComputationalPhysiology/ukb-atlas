3ffd8b960a96959a5e3273fc4aa385051bd7c4cb
Henrik Finsberg <henrikfinsberg@hotmail.com>
2025-03-31 08:42:30 +0200

    Add functionality to clip mesh

--- docs/demo_dolfinx.py
+++ docs/demo_dolfinx.py
@@ -1,21 +1,47 @@
 # # Example with dolfinx
 
+import sys
+
+sys.path.remove("/opt/homebrew/lib")
 import subprocess
 from pathlib import Path
 from mpi4py import MPI
 import pyvista
-import dolfinx
 import gmsh
+import dolfinx
 
 
-# First we try to generate the mean shape from the UK Biobank atlas
+# Let us first list the help menu
 
-folder = Path("data")
-subdir = folder / "mean"
-subprocess.run(["ukb-atlas", str(subdir), "--mesh"])
+subprocess.run(["ukb-atlas", "--help"])
+
+# We will first use the `surf` command
+
+subprocess.run(["ukb-atlas", "surf", "--help"])
+
+# First we try to generate the surfaces for mean shape from the UK Biobank atlas end-diastole
+
+folder = Path("data-mean")
+mode = "-1"  # -1 refers to mean shape (otherwise you can choice a value between 0 and 200)
+std = "1.5"  # Standard deviation to scale the mode by, by default 1.5
+subprocess.run(["ukb-atlas", "surf", str(folder), "--mode", mode, "--std", std, "--case", "ED"])
+
+# Let us now see which files that was generated
+
+subprocess.run(["ls", str(folder)])
+
+# We will now generate the mesh for the mean shape, but let us first look at the help menu
+
+subprocess.run(["ukb-atlas", "mesh", "--help"])
+
+# Now we will generate the mesh for the mean shape (let us pick the rest of the parameters as default)
+
+subprocess.run(["ukb-atlas", "mesh", str(folder)])
+
+# Note that by decreasing the values of `char_length_max`and `char_length_min` you will get a finer mesh. We can now load the mesh with dolfinx and plot it with pyvista
 
 comm = MPI.COMM_WORLD
-msh_file = subdir / "ED.msh"
+msh_file = folder / "ED.msh"
 gmsh.initialize()
 gmsh.model.add("Mesh from file")
 gmsh.merge(str(msh_file))
@@ -25,6 +51,7 @@ markers = {
 }
 print(markers)
 gmsh.finalize()
+
 
 pyvista.start_xvfb()
 vtk_mesh = dolfinx.plot.vtk_mesh(mesh, mesh.topology.dim)
@@ -51,11 +78,18 @@ else:
 
 
 # Now lets us perturb the mean shape with the second mode
-subdir = folder / "mode_1"
-subprocess.run(["ukb-atlas", str(subdir), "--mesh", "--mode", "1", "--std", "1.5"])
+
+folder_mode_1 = Path("data-mode-1")
+subprocess.run(
+    ["ukb-atlas", "surf", str(folder_mode_1), "--mode", "1", "--std", "1.5", "--case", "ED"]
+)
+
+# And create the mesh
+
+subprocess.run(["ukb-atlas", "mesh", str(folder_mode_1)])
 
 comm = MPI.COMM_WORLD
-msh_file = subdir / "ED.msh"
+msh_file = folder_mode_1 / "ED.msh"
 gmsh.initialize()
 gmsh.model.add("Mesh from file")
 gmsh.merge(str(msh_file))
@@ -88,4 +122,116 @@ p2.add_mesh(bgrid2, show_edges=True)
 if not pyvista.OFF_SCREEN:
     p2.show()
 else:
-    figure = p.screenshot("facet_tags_biv.png")
+    figure = p2.screenshot("facet_tags_biv.png")
+
+
+# ## Clipping the mesh
+#
+# Let us now show how to clip the mesh. First let us just clip the first mesh using `pyvista` to see how we can the mesh to look like. This is also a good way to figure out what should be the origin and normal for the clip plane. (Note that the default values are the ones given below which works well for the mean shape of the ED mesh)
+
+origin = (-13.612554383622273, 18.55767189380559, 15.135103714006394)
+normal = (-0.7160843664428893, 0.544394641424108, 0.4368725838557541)
+p3 = pyvista.Plotter()
+p3.add_mesh_clip_plane(grid, normal=normal, origin=origin, invert=True)
+if not pyvista.OFF_SCREEN:
+    p3.show()
+else:
+    figure = p3.screenshot("mesh_clip.png")
+
+# There is a command called `clip`, so let us first look at the help section
+
+subprocess.run(["ukb-atlas", "clip", "--help"])
+
+# Note that this command uses `pyvista`, which is not part of the default dependencies so this needs to be installed. You can do this using either
+# ```
+# python3 -m pip install pyvista
+# ```
+# or use
+# ```
+# python3 -m pip install ukb-atlas[pyvista]
+# ```
+#
+# To create the mesh we pass the origin and normal, we will also smooth the RV endocardium because the original surface has some very sharp transistions
+
+subprocess.run(
+    [
+        "ukb-atlas",
+        "clip",
+        str(folder),
+        "--smooth",
+        "--case",
+        "ED",
+        "-ox",
+        str(origin[0]),
+        "-oy",
+        str(origin[1]),
+        "-oz",
+        str(origin[2]),
+        "-nx",
+        str(normal[0]),
+        "-ny",
+        str(normal[1]),
+        "-nz",
+        str(normal[2]),
+    ]
+)
+
+# Let us again see which files we have
+
+subprocess.run(["ls", str(folder)])
+
+# There are now some new `.ply` files that represents the clipped surfaces. We can for example take a look at one of them
+
+# +
+lv_clipped = pyvista.read(folder / "lv_clipped.ply")
+
+p4 = pyvista.Plotter()
+p4.add_mesh(lv_clipped)
+if not pyvista.OFF_SCREEN:
+    p4.show()
+else:
+    figure = p4.screenshot("lv_clipped.png")
+# -
+
+# To mesh to three surfaces togeher we repeat the mesh command but pass the `--clipped` flag
+
+subprocess.run(["ukb-atlas", "mesh", str(folder), "--clipped"])
+
+# We can now load the new clipped mesh into dolfinx
+
+comm = MPI.COMM_WORLD
+msh_file = folder / "ED_clipped.msh"
+gmsh.initialize()
+gmsh.model.add("Mesh from file")
+gmsh.merge(str(msh_file))
+mesh_clipped, ct_clipped, ft_clipped = dolfinx.io.gmshio.model_to_mesh(gmsh.model, comm, 0)
+markers_clipped = {
+    gmsh.model.getPhysicalName(*v): tuple(reversed(v)) for v in gmsh.model.getPhysicalGroups()
+}
+print(markers_clipped)
+gmsh.finalize()
+
+# and visualize it
+
+pyvista.start_xvfb()
+vtk_mesh_clipped = dolfinx.plot.vtk_mesh(mesh_clipped, mesh_clipped.topology.dim)
+grid_clipped = pyvista.UnstructuredGrid(*vtk_mesh_clipped)
+p5 = pyvista.Plotter()
+p5.add_mesh(grid_clipped, show_edges=True)
+# p5.add_mesh(grid, style="wireframe", color="k")
+p5.view_xy()
+if not pyvista.OFF_SCREEN:
+    p5.show()
+else:
+    figure = p5.screenshot("mesh_clipped.png")
+
+vtk_bmesh_clipped = dolfinx.plot.vtk_mesh(mesh_clipped, ft_clipped.dim, ft_clipped.indices)
+bgrid_clipped = pyvista.UnstructuredGrid(*vtk_bmesh_clipped)
+bgrid_clipped.cell_data["Facet tags"] = ft_clipped.values
+bgrid_clipped.set_active_scalars("Facet tags")
+p6 = pyvista.Plotter(window_size=[800, 800])
+p6.add_mesh(bgrid_clipped, show_edges=True)
+if not pyvista.OFF_SCREEN:
+    p6.show()
+else:
+    figure = p6.screenshot("facet_tags_biv_clipped.png")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,7 +6,7 @@ import ukb.surface
 
 @pytest.mark.parametrize("case", ["ED", "ES", "both"])
 def test_generate_surfaces_mean_healthy(case, tmp_path):
-    ukb.cli.main([str(tmp_path), "--case", case])
+    ukb.cli.main(["surf", str(tmp_path), "--case", case])
     assert (tmp_path / "parameters.json").exists()
 
     if case == "both":
@@ -26,10 +26,29 @@ def test_generate_surfaces_mean_healthy(case, tmp_path):
             assert not path.exists()
 
 
+def test_generate_mesh(tmp_path):
+    ukb.cli.main(["surf", str(tmp_path), "--case", "ED"])
+    assert not (tmp_path / "ED.msh").exists()
+    ukb.cli.main(["mesh", str(tmp_path), "--case", "ED"])
+    assert (tmp_path / "ED.msh").exists()
+
+
+def test_clip_mesh(tmp_path):
+    ukb.cli.main(["surf", str(tmp_path), "--case", "ED"])
+    assert not (tmp_path / "lv_clipped.ply").exists()
+    ukb.cli.main(["clip", str(tmp_path), "--case", "ED", "--smooth"])
+    assert (tmp_path / "lv_clipped.ply").exists()
+    assert (tmp_path / "rv_clipped.ply").exists()
+    assert (tmp_path / "epi_clipped.ply").exists()
+    assert not (tmp_path / "ED_clipped.msh").exists()
+    ukb.cli.main(["mesh", str(tmp_path), "--case", "ED", "--clipped"])
+    assert (tmp_path / "ED_clipped.msh").exists()
+
+
 def test_generate_surfaces_non_mean_healthy(tmp_path):
     mode = 1
     std = 0.3
-    ukb.cli.main([str(tmp_path), "--mode", str(mode), "--std", str(std)])
+    ukb.cli.main(["surf", str(tmp_path), "--mode", str(mode), "--std", str(std), "--case", "both"])
 
     assert (tmp_path / "parameters.json").exists()
     params = json.loads((tmp_path / "parameters.json").read_text())
@@ -44,7 +63,7 @@ def test_generate_surfaces_non_mean_healthy(tmp_path):
 
 @pytest.mark.xfail(reason="I think we have the wrong template")
 def test_generate_surfaces_mean_all(tmp_path):
-    ukb.cli.main([str(tmp_path), "--all", "--subdir", "."])
+    ukb.cli.main(["surf", str(tmp_path), "--all", "--case", "both"])
     assert (tmp_path / "UKBRVLV_ALL.h5").exists()
     assert (tmp_path / "parameters.json").exists()
     for name in ukb.surface.surfaces:
